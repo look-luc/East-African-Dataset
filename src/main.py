79273f99ac971761e104788e276de4f10e7c2551
look-luc <denardiluc@gmail.com>
2026-06-29 18:24:54 -0600

    gathered data

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import sys
+from pathlib import Path
+
+import pandas as pd
+
+root_dir = Path(__file__).resolve().parents[3]
+if str(root_dir) not in sys.path:
+    sys.path.append(str(root_dir))
+
+from data.get_data import Get_Data
+
+
+def main():
+    data = Get_Data().to_csv("data.csv", sep='\t')
+
+    print("gathered data into data.csv")
