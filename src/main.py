578798c82a8299959e7fbca456d22814000e390d
look-luc <denardiluc@gmail.com>
2026-07-05 14:27:15 -0400

    added new translation

--- src/main.py
+++ src/main.py
@@ -21,6 +21,6 @@ def main(path, task:str, lang:str):
 
         print("finished counting morphemes")
     elif task=="morpheme_translate":
-        translation(lang, lang)
+        translation(lang, lang, "data")
     elif task == "model_segment":
         model_extract("data.csv", lang)
