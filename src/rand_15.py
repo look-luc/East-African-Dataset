2bde0c8514aeca3a918d9a9154dfd1a9edb1ce87
look-luc <denardiluc@gmail.com>
2026-07-09 06:03:09 -0400

    Update rand_15.py

--- src/rand_15.py
+++ src/rand_15.py
@@ -1,15 +1,19 @@
+from pathlib import Path
+
 import pandas as pd
 
+script_path = Path(__file__).resolve().parent.parent
+
 languages = ["ganda", "gikuyu", "tshiluba", "chiga", "tooro", "runyoro", "kamba"]
-data_df = pd.read_csv("/Users/lucdenardi/Desktop/code_lang/python/East-African-Dataset/data/data.csv", sep='\t')
+data_df = pd.read_csv(f"{script_path}/data/data.csv", sep='\t')
 
 for lang in languages:
     lang_df = data_df[data_df["language"]==lang]
     if lang == "ganda":
-        fig_rand_rows = pd.DataFrame(lang_df[lang_df["Output Type"]=="figurative_translate"]).sample(n=17)
-        lit_rand_rows = pd.DataFrame(lang_df[lang_df["Output Type"]=="leteral_translate"]).sample(n=17)
+        fig_rand_rows = pd.DataFrame(lang_df[lang_df["Output Type"]=="figurative_translate"]).sample(n=42)
+        lit_rand_rows = pd.DataFrame(lang_df[lang_df["Output Type"]=="leteral_translate"]).sample(n=42)
     else:
-        fig_rand_rows = pd.DataFrame(lang_df[lang_df["Output Type"]=="figurative_translate"]).sample(n=15)
-        lit_rand_rows = pd.DataFrame(lang_df[lang_df["Output Type"]=="leteral_translate"]).sample(n=15)
+        fig_rand_rows = pd.DataFrame(lang_df[lang_df["Output Type"]=="figurative_translate"]).sample(n=40)
+        lit_rand_rows = pd.DataFrame(lang_df[lang_df["Output Type"]=="leteral_translate"]).sample(n=40)
     fig_rand_rows.to_csv(f"fig_{lang}_random_15.csv", sep='\t')
     lit_rand_rows.to_csv(f"lit_{lang}_random_15.csv", sep='\t')
