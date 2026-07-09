f9d342970fa3dbb8b9dcec8ea11f8cb363ce4b6e
look-luc <denardiluc@gmail.com>
2026-07-09 15:08:06 -0400

    update output
    Signed-off-by: look-luc <denardiluc@gmail.com>

--- data/get_data.py
+++ data/get_data.py
@@ -13,7 +13,7 @@ bantu_langs = ["ganda", "gikuyu", "tshiluba", "chiga", "tooro", "runyoro", "kamb
 def making_df(jsonl_list):
     lang_pattern = r"\*\*Input\*\*:\nA proverb in (.*?)\n\n\*\*Output\*\*:"
     pattern = r"Now, please translate the following proverb:\n\n\*\*Input\*\*:\n(.*?)\n\n\*\*Output\*\*:"
-    output_pattern = r"\*\*Output\*\*:<end_of_turn>\n<start_of_turn>model\n\", \"predict\": \"(.*?)\", \"label\"\: \"Avoid the fight which is not yours\.\""
+    output_pattern = r"\"(.*?)\""
 
     df = []
     for file_path in jsonl_list:
@@ -52,7 +52,7 @@ def making_df(jsonl_list):
     final_df["african_proverb"] = final_df["prompt"].str.extract(pattern, flags=re.DOTALL, expand=False)
     final_df["african_proverb"] = final_df["african_proverb"].str.strip()
 
-    final_df["model output"] = final_df["prompt"].str.extract(output_pattern, flags=re.DOTALL, expand=False)
+    final_df["model output"] = final_df["predict"].str.extract(output_pattern, flags=re.DOTALL, expand=False)
 
     return final_df[["experiment_config", "language", "language_family", "african_proverb", "label", "Output Type", "model output"]]
 
